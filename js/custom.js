c56f456045aeebc57b7aa390b22f8cb75d70b916
vrobledo <vrobledo@simplestate.com.ar>
2022-05-25 18:30:51 -0300

    first

---
+++ js/custom.js
@@ -0,0 +1,90 @@
+$(window).scroll(function() {
+    var height = $(window).scrollTop();
+
+    if(height  > 550) {
+        $('.home-desc').addClass('sticky');
+        $('.home-desc ul').addClass('stickyUl');
+    }else {
+    	$('.home-desc').removeClass('sticky');
+      $('.home-desc ul').removeClass('stickyUl');
+    }
+});
+$('.navbar-nav>li>a').on('click', function(){
+	$('.navbar-collapse').collapse('hide');
+});
+$(document).ready(function(){
+	$('a[href^="#"]').on('click',function (e) {
+		e.preventDefault();
+
+		var target = this.hash;
+		var $target = $(target);
+
+		$('html, body').stop().animate({
+			'scrollTop': $target.offset().top
+		}, 900, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+	$('body').scrollspy({ target: '.menu-principal' });
+	$('#capa-tabs a').on('click', function (e) {
+	  e.preventDefault()
+	  $('#capa-tabs a').removeClass('active');
+	  $(this).tab('show')
+	  $(this).addClass('active')
+	});
+
+  $('#idiomas').on('change', function () {
+    var url = $(this).val(); 
+    if (url) {
+      window.location = url;
+    }
+  })
+
+});
+$(document).ready(function(){
+  $(".owl-testimonios").owlCarousel({
+  	nav:true,
+  	loop:true,
+  	autoplay:true,
+  	autoplayHoverPause:true,
+  	responsive:{
+        0:{
+            items:1,
+            dots:false,
+            nav:false
+        },
+        600:{
+            items:1,
+            dots:false,
+            nav:false
+        },
+        1000:{
+            items:1,
+            dots:false
+        }
+    }
+  });
+  $(".owl-personas").owlCarousel({
+    nav:true,
+    loop:true,
+    slideBy:5,
+    autoplay:true,
+    autoplayHoverPause:true,
+    responsive:{
+        0:{
+            items:2,
+            dots:false,
+            nav:false
+        },
+        600:{
+            items:5,
+            dots:false,
+            nav:false
+        },
+        1000:{
+            items:5,
+            dots:false
+        }
+    }
+  });
+});
---
+++ js/testimonios.js
@@ -0,0 +1,156 @@
+new Vue({
+  el: '#app',
+  data: {
+    perfiles: [
+      {
+        img: 't1',
+        nombre: 'Pabla Adiana Santacruz Moran',
+        oficio: 'Entrenadora Personal e Instructora de Yoga',
+        texto:
+          'Musculación inteligente Es un libro muy didáctico, y de mucha utilidad para toda persona que realiza actividad física! Excelente libro',
+      },
+      {
+        img: 't2',
+        nombre: 'Alexander Maidana',
+        oficio: 'Preparador Físico',
+        texto:
+          'Ya son varios años que llevo aprendiendo de uno de los mejores en esta materia, Nicolás es mucho más que un excelente profesor, es un gran ser humano comprometido al máximo con su profesión y sus valores. Estoy muy agradecido por todo lo aprendido en estos 3 cursos en los que he podido disfrutar de su trabajo. Quien pueda darse la oportunidad de leer su libro, comprenderá muy bien estas palabras, es en verdad un excelente material no solo de estudio, sino también para poner en práctica. Querido amigo, profesor y colega gracias por ponerle tanta dedicación, tanta pasión a lo que amas, y por darnos la posibilidad de ser parte de ello. Por muchos éxitos más!!!',
+      },
+      {
+        img: 't3',
+        nombre: 'Facundo Solano',
+        oficio: 'Instructor de Musculación Y Músico',
+        texto:
+          'Muy buen libro, excelente la forma en que están explicados los ejercicios y los tips de entrenamiento ,día a día en el gym aplicando en forma práctica la teoría del libro de musculación inteligente.',
+      },
+      {
+        img: 't4',
+        nombre: 'Damián Benítez',
+        oficio: 'Policía de la Ciudad de Buenos Aires',
+        texto:
+          'Gracias por compartir tus conocimientos en este excelente libro lo llevo a toda partes , Muy recomendado gran persona y un genio como profe.',
+      },
+      {
+        img: 't5',
+        nombre: 'Alicia Segovia',
+        oficio: 'Instructora de técnicas de gimnasia',
+        texto:
+          'Me parece un libro genial... me encanto ...excelente, fácil de entender. Nicolás es un gran profesional de su trabajo ..un gran entrenador ... mil gracias Nico...!! por brindar todos tus conocimientos ...y me alegro mucho de haberte tenido como profesor ...saludos y éxitos ....',
+      },
+      {
+        img: 't6',
+        nombre: 'Joan Torino',
+        oficio: 'Preparador Físico e Instructor de natación',
+        texto:
+          'Un entrenador que busca el conocimiento donde quiera que este, esta condenado al éxito , Musculación Inteligente sirve tanto a los que ya somos entrenadores como a los alumnos para comprender mejor las muchas variables del entrenamiento, es de fácil interpretación y de mucha calidad su contenido, no deja excusas para empezar a entrenar!!!',
+      },
+      {
+        img: 't7',
+        nombre: 'Diego Delfino',
+        oficio: 'Jugador federado de Ping Pong',
+        texto:
+          'Excelente libro, lenguaje claro y preciso. Aporta los conocimientos imprescindibles que a todos los que nos gusta el deporte necesitamos saber!!!',
+      },
+      {
+        img: 't8',
+        nombre: 'Mariana Salas Correa',
+        oficio: 'Entrenadora Personal y Preparadora de Fitness de competición',
+        texto:
+          'Excelente!! Felicitaciones Nico! Me encanto, muy didáctico y fácil de entender, lo recomiendo a mis alumnos. Abrazo',
+      },
+      {
+        img: 't9',
+        nombre: 'Ahmed Molina',
+        oficio: 'Estudiante de Licenciatura en Kinesiología',
+        texto:
+          'Un libro extraordinario, la enseñanza que te da y las dudas que te surgen cuando empezas el mundo de la pesas, al leerlo te da las bases necesarias para tener un entrenamiento de verdad. Es muy admirable como profesor y entrenador, tuve la suerte de aprender de el y sigo aprendiendo todos los conocimientos que nos brinda en los curso de musculación y entrenamiento personal.',
+      },
+      {
+        img: 't10',
+        nombre: 'Adriana Ponce',
+        oficio: 'Médica y atleta especialista en Running',
+        texto: 'Excelente libro, mucha dedicación y pasión en cada capítulo.',
+      },
+      {
+        img: 't11',
+        nombre: 'Wilfredo Pinares',
+        oficio: 'Entrenador Personal y Preparador Físico',
+        texto:
+          'Muy buen libro, didáctico y fácil de entender .. parece ser que la mayoría insiste en explicar difícil .. pero usted comprende que hay que hacer lo contrario para cumplir con el propósito de enseñar y dejarse entender. Agradezco desde ya tan buen libro y lo esperamos pronto dando una capacitación aquí en Perú',
+      },
+      {
+        img: 't12',
+        nombre: 'Gaspar Mangas',
+        oficio:
+          'Profesor Nacional de Educación Física y Preparador Físico de Fútbol',
+        texto:
+          'Gracias Nico por ser un excelente profesor y plasmar tus conocimientos en estos libros que son una maravilla!!! Abrazooo grande!!!',
+      },
+      {
+        img: 't13',
+        nombre: 'Sofia Yanel Velasquez',
+        oficio: 'Atleta Fitness',
+        texto: 'Muy buen libro, fácil de entender!!!',
+      },
+      {
+        img: 't14',
+        nombre: 'Matias Marchetti',
+        oficio:
+          'Licenciado en Nutrición especializado en actividad física y deportes',
+        texto:
+          'Muy buen libro para estudiantes. La información es clara y llevadera, leí ambos libros y se los vi a un colega que estoy formando para trabajar en el gimnasio. ',
+      },
+      {
+        img: 't15',
+        nombre: 'Ezequiel Millet',
+        oficio:
+          'Atleta de Levantamiento de Potencia y Técnico Superior en Actividad Física y Deportes',
+        texto:
+          'Un honor contar con una edición tan excelente y demandada. Información muy clara y directa.',
+      },
+    ],
+    personas: [
+      { img: '1' },
+      { img: '2' },
+      { img: '4' },
+      { img: '5' },
+      { img: '6' },
+      { img: '7' },
+      { img: '8' },
+      { img: '9' },
+      { img: '10' },
+      { img: '11' },
+      { img: '12' },
+      { img: '13' },
+      { img: '14' },
+      { img: '15' },
+      { img: '16' },
+      { img: '17' },
+      { img: '18' },
+      { img: '19' },
+      { img: '20' },
+      { img: '22' },
+      { img: '23' },
+      { img: '24' },
+      { img: '25' },
+      { img: '26' },
+      { img: '27' },
+      { img: '28' },
+      { img: '29' },
+      { img: '30' },
+      { img: '31' },
+      { img: '32' },
+      { img: '33' },
+      { img: '34' },
+      { img: '35' },
+      { img: '36' },
+      { img: '37' },
+      { img: '38' },
+      { img: '39' },
+      { img: '40' },
+      { img: '41' },
+      { img: '42' },
+      { img: '43' },
+    ],
+  },
+});
